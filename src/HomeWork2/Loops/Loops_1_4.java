6d656fa2ca4f8815c65b664c9533b839b5637823
Sergey Evtyushyn <sergey.evtyushyn@gmail.com>
2022-04-04 22:05:42 +0300

    Добавил задание 1.4

--- src/HomeWork2/Loops/Loops_1_4.java
+++ src/HomeWork2/Loops/Loops_1_4.java
@@ -4,33 +4,25 @@ import java.util.Scanner;
 
 public class Loops_1_4 {
     public static void main(String[] args) {
-        //Scanner an = new Scanner(System.in);
-        //System.out.println("Введите число умножения:");
-        //long n = an.nextLong();
-        int n = 3;
-        long a = 1;
+        Scanner an = new Scanner(System.in);
+        System.out.println("Введите целое число для умножения:");
+        double n1;
 
-        while ( a <= Long.MAX_VALUE ) { //|| a >= -9_223_372_036_854_775_808L
-
-             a = a * n;
-
-            //if (a > Long.MAX_VALUE){
-            //    System.out.println("Значение до переполенения" + a / n);
-            //    System.out.println("Значение после переполенения" + a);
-            //} else if (a < -9_223_372_036_854_775_808l){
-            //    System.out.println("Значение до переполенения" + a / n);
-            //    System.out.println("Значение после переполенения" + a);
-            //}
-
-
+        while (true){
+            n1 = an.nextDouble();
+            if (n1 != (long)n1) {
+                System.out.println("Введено не целое число. Повторите ввод:");
+            } else break;
         }
-        System.out.println(a);
-        //System.out.println(a);
+        long n = (long)n1;
+        long a = 1;
+        long c = Long.MAX_VALUE / n;
+        while ( a <= Math.abs(c)  ) {
+             a *= n;
+        }
+        System.out.println("Переполнение.");
+        System.out.println("Результат умножения до переполнения: " + a);
+        System.out.println("Результат умножения до переполнения: " + a * n);
 
-        //float a_n = 1;
-       //for (int i = 0; i < n; i++) {
-          //  a_n = a_n * a;
-        //}
-        //System.out.println(a + " ^ " + n + " = " + a_n);
     }
 }
