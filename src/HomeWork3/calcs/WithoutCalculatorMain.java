3bbc94d55538c6c6ff9c3295bdbff0c51be1e325
Sergey Evtyushyn <sergey.evtyushyn@gmail.com>
2022-04-11 22:08:50 +0300

    ДЗ_3 задание 1

---
+++ src/HomeWork3/calcs/WithoutCalculatorMain.java
@@ -0,0 +1,9 @@
+package HomeWork3.calcs;
+
+public class WithoutCalculatorMain {
+    public static void main(String[] args) {
+        double a = 4.1 + 15 * 7 + Math.pow((28 / (double) 5), 2); // Результат 140,45999999999998
+        System.out.println("a = " + a);
+
+    }
+}
