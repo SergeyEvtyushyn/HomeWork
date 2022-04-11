24162928104d105cd68d436e3b34df48accaecf4
Sergey Evtyushyn <sergey.evtyushyn@gmail.com>
2022-04-11 22:06:02 +0300

    Добавлено задание 4.1.2 из ДЗ_2

--- src/HomeWork2/Sorts/SortsUtils.java
+++ src/HomeWork2/Sorts/SortsUtils.java
@@ -4,12 +4,19 @@ import java.util.Arrays;
 
 public class SortsUtils {
     public static void main(String[] args) {
-        int[] arr = {4, 3, 5, 1, 2};
+        int[] arr = {4, 3, 5, 1, 2, 6, 8, 9, 7 , 0};
 
         int[] bubble = bubble(arr);
+        System.out.println();
+        int[] shake = shake(arr);
 
     }
 
+    /**
+     * Метод пузырьковой сортировки
+     * @param arr вводимый параметр из массива целых чисел
+     * @return отсортированный массив по возрастанию
+     */
 
     public static int[] bubble(int[] arr){
         int x;
@@ -28,4 +35,38 @@ public class SortsUtils {
         return arr;
     }
 
+    /**
+     * Метод шэйкер-сортировки
+     * @param arr вводимый параметр из массива целых чисел
+     * @return отсортированный массив по убыванию
+     */
+    public static int[] shake(int[] arr){
+        int r = arr.length - 1;
+        int l = 0;
+        int x;
+        int flag = 1;
+        while (l < r && flag != 0) {
+            flag = 0;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if ( arr[i] < arr[i+1] ){
+                    x = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = x;
+                    flag = 1;
+                }
+            }
+        r--;
+            for (int i = r; i > l ; i--) {
+                if ( arr[i - 1] < arr[i]){
+                    x = arr[i];
+                    arr[i] = arr[i-1];
+                    arr[i-1] = x;
+                    flag = 1;
+                }
+            }
+        l++;
+        }
+        System.out.print(Arrays.toString(arr) + " ");
+        return arr;
+    }
 }
