f5379a48fb57af77694b90be081a5bcbb59b494d
Sergey Evtyushyn <sergey.evtyushyn@gmail.com>
2022-05-15 17:24:24 +0300

    ДЗ 5. Задания 2.1 и 2.2

--- src/HomeWork5/bookRead.java
+++ src/HomeWork5/bookRead.java
@@ -1,51 +1,48 @@
 package HomeWork5;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 
 public class bookRead {
-
-    public static void main(String[] args) throws IOException {
+       public static void main(String[] args) {
 
         // Создание объекта файла с указанием абсолютного пути к файлу
         File file = new File("C:\\Java\\JC1\\Md-JC1\\HomeWork\\src\\HomeWork5\\Война и мир_книга.txt");
 //        File file = new File("C:\\Java\\JC1\\Md-JC1\\HomeWork\\src\\HomeWork5\\Test.txt"); // Для тестирования
 //        File file = new File("C:\\Java\\JC1\\Md-JC1\\HomeWork\\src\\HomeWork5\\Test1.txt"); // Для тестирования
 
-        // Создание объекта для чтения файла с обработкой исключения, если файл не найдет в указанном месте
-        BufferedReader wptxt  = null;
-        try {
-            wptxt = new BufferedReader(new FileReader(file));
+        // Создание объекта для чтения файла с обработкой исключения, если файл не найден в указанном месте
+        String[] fileToString = null;
+        try (BufferedReader wptxt = new BufferedReader(new FileReader(file))) {
+            fileToString = fileToString(wptxt);
         } catch (FileNotFoundException e) {
             System.out.println("Указанный файл не найден.");
+        } catch (IOException e) {
+            System.out.println("Не могу прочитать файл");
         }
 
-        // Создание строки из прочитанного текста в файле.
+        // Помещение слов из текста в коллекцию Set и обработка ее.Задание 2.1
+        HashSet<String> wpset = new HashSet<>(List.of(fileToString));
+
+        System.out.println("Файл с текстом: " + file.getName());
+        System.out.println("Количество уникальных слов в тексте: " + wpset.size());
+    }
+
+    /**
+     * Метод обработки файла и помещение текста в строку
+     * @param wptxt Чтение из файла с применением BufferedRead
+     * @return Массив слов в виде строки
+     * @throws IOException проверка исключений в мэйне, в месте вызова метода
+     */
+    public static String[] fileToString(BufferedReader wptxt) throws IOException {
         StringBuilder alltext = new StringBuilder();
         int txtIn = wptxt.read();
         while ( txtIn != -1) {
-           alltext.append((char)txtIn);
-//            System.out.print((char)txtIn);
+            alltext.append((char)txtIn);
             txtIn = wptxt.read();
         }
-        String s = alltext.toString();
-        String[] s2 = s.split("[ .,!?:;]+"); // TODO... Проверить на правильность принятия всех условий для слов со знаками
-
-        // Помещение слов из текста в коллекцию Set и обработка ее.Задание 2.1
-        HashSet<String> wpset = new HashSet<String>();
-            wpset.addAll(List.of(s2));
-       for (String elem : wpset){
-           System.out.print(" " + elem);
-       }
-
-       System.out.println();
-       System.out.println("Количество уникальных слов в тексте: " + wpset.size());
-
-
-        wptxt.close(); // Закрытие ранее открытого файла
+        return alltext.toString().split("[.,!?:;\\s]+"); // TODO... Проверить на правильность принятия всех условий для слов со знаками
     }
 }
--- src/HomeWork5/bookReadMAP.java
+++ src/HomeWork5/bookReadMAP.java
@@ -1,70 +1,83 @@
 package HomeWork5;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
-public class bookReadMAP {
+public class bookReadMAP extends bookRead {
+   
+    public static void main(String[] args) {
 
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-
-    public static void main(String[] args) throws IOException {
-
-
+        Scanner sc = new Scanner(System.in);
 
         // Создание объекта файла с указанием абсолютного пути к файлу
         File file = new File("C:\\Java\\JC1\\Md-JC1\\HomeWork\\src\\HomeWork5\\Война и мир_книга.txt");
 //        File file = new File("C:\\Java\\JC1\\Md-JC1\\HomeWork\\src\\HomeWork5\\Test.txt"); // Для тестирования
 //        File file = new File("C:\\Java\\JC1\\Md-JC1\\HomeWork\\src\\HomeWork5\\Test1.txt"); // Для тестирования
 
-        // Создание объекта для чтения файла с обработкой исключения, если файл не найдет в указанном месте
-        BufferedReader wptxt  = null;
-        try {
-            wptxt = new BufferedReader(new FileReader(file));
+        // Создание объекта для чтения файла с обработкой исключения, если файл не найден в указанном месте
+        String[] fileToString = null;
+        try (BufferedReader wptxt = new BufferedReader(new FileReader(file))) {
+            fileToString = bookRead.fileToString(wptxt);
         } catch (FileNotFoundException e) {
             System.out.println("Указанный файл не найден.");
+        } catch (IOException e) {
+            System.out.println("Не могу прочитать файл");
         }
 
-        // Создание строки из прочитанного текста в файле.
-        StringBuilder alltext = new StringBuilder();
-        int txtIn = wptxt.read();
-        while ( txtIn != -1) {
-           alltext.append((char)txtIn);
-//            System.out.print((char)txtIn);
-            txtIn = wptxt.read();
+        System.out.println("Какое количество топ слов выводить?");
+        int n = sc.nextInt();
+
+        System.out.println("Файл с текстом: " + file.getName());
+
+        Map<String, Integer> sortedwpputall = sortedwpputall(fileToString);
+        int i = 0;
+        // Вывод n-задаваемых значений после сортировки задание. Задание 2.2
+        for(Map.Entry<String, Integer> entry : sortedwpputall.entrySet()){
+            if ( i == n ){
+                break;
+            } else {
+                String key = entry.getKey();
+                int value = entry.getValue();
+                System.out.println("Количество повторений слова \"" + key + "\" составляет: " + value + " раз.");
+                i++;
+            }
         }
-        String s = alltext.toString();
-        String[] s2 = s.split("[ .,!?:;]+"); // TODO... Проверить на правильность принятия всех условий для слов со знаками
+    }
 
-        Map wpputall = new HashMap();
+    /**
+     * Метод наполнения колллекции Мар с сортировкой по значению
+     * @param fileToString Массив слов
+     * @return Сортированнаяколлекция по знпчению
+     */
+    public static Map<String, Integer> sortedwpputall(String[] fileToString){
+        Map<String, Integer> wpputall = new HashMap<>();
 
- //       for (int i = 0; i < s2.length; i++) {
- //           wpputall.forEach(Object o : wpputall);
- //           if (s2[i].equals(wpputall.get(o)))
- //       }
+        for (String s1 : fileToString) {
+            wpputall.put(s1, 0);
+        }
 
-        // Помещение слов из текста в коллекцию Set и обработка ее.Задание 2.1
-//        HashSet<String> wpset = new HashSet<String>();
-//            wpset.addAll(List.of(s2));
-//       for (String elem : wpset){
-//           System.out.print(" " + elem);
-//       }
+        for (String s1 : fileToString) {
+            int value = wpputall.get(s1);
+            wpputall.put(s1, value + 1);
+        }
 
-//       System.out.println();
-//       System.out.println("Количество уникальных слов в тексте: " + wpset.size());
+        //Сортировка Мар по значению в порядке убывания
+        List <Map.Entry<String, Integer>> valuesList = new ArrayList(wpputall.entrySet());
+        Collections.sort(valuesList, new Comparator<Map.Entry<String, Integer>>(){
 
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
 
-        wptxt.close(); // Закрытие ранее открытого файла
+        //Содание новой коллекции Мар и сохраннение в ней отсортированных значений
+        Map<String, Integer> sortedwpputall = new LinkedHashMap<>();
+
+        for (Map.Entry<String, Integer> entry : valuesList){
+            sortedwpputall.put(entry.getKey(), entry.getValue());
+        }
+        return sortedwpputall;
     }
 }
