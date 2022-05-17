40a65829556fa3e76838927de7ef6c1a226a0c2b
Sergey Evtyushyn <sergey.evtyushyn@gmail.com>
2022-05-17 22:06:25 +0300

    ДЗ 5. Задание 4.1 с правками

--- src/HomeWork5/EasySearch.java
+++ src/HomeWork5/EasySearch.java
@@ -5,18 +5,67 @@ import java.util.Map;
 public class EasySearch implements ISearchEngine{
 
 
+    /**
+     * Метод поиска слов в тексте без применения регулярных выражений
+     * @param text Текст, в котором ищем
+     * @param word Искомое слово
+     * @return Возвращает число совпадений. При отсутсвии совападений возвращается -1.
+     */
     @Override
-    public long search(String text, String word){  // TODO... Добавить вариант для случая поиска союза.
+    public long search(String text, String word){
+        String[] s = {};
         long count = 0;
-        long index = text.indexOf(word);
-        if (index == -1 ){
-            return -1;
-        } else {
-            while (index != -1){
-                index = text.indexOf(word, (int) (index + 1));
-                count++;
+
+        if (word.length() == 1) { //Часть для поиска союзов или слов в одну букву
+            s = new String[]{" " + word + " ", " " + word + ".", " " + word + ",", " " + word + ":", " " + word + ";",
+                    word + " ", word + ".", word + ",", word + ":", word + ";", " " + word};
+
+            long index = text.indexOf(word);
+            for (String words : s) {
+                while (index != -1) {
+                    index = text.indexOf(word, (int) (index + 1));
+                    count++;
+                }
             }
-            return count;
-        }
+            if (count == 0) {
+                return -1;
+            }
+
+        } else { //Часть для поиска целых слов
+            long index = 0;
+            long index1 = 0;
+
+            while (index != -1) {
+                index = text.indexOf(word, (int) index1);
+                index1 = index + 1;
+                boolean a = index > 0;
+                boolean b = (index + word.length()) < text.length();
+
+
+                if ((a && b)) {
+                    char c1 = text.charAt((int)(index - 1));
+                    char c2 = text.charAt((int)(index + word.length()));
+                    if (!Character.isDigit(c1) && !Character.isLetter(c1) && !Character.isDigit(c2) && !Character.isLetter(c2) ){
+                        count++;
+                    }
+                } else if (a && !b) {
+                    char c1 = text.charAt((int)(index - 1));
+                    if (!Character.isDigit(c1) && !Character.isLetter(c1) ){
+                        count++;
+                    }
+
+                } else if ( !a && b) {
+                    char c2 = text.charAt((int)(index + word.length()));
+                    if ( !Character.isDigit(c2) && !Character.isLetter(c2) ){
+                        count++;
+                    }
+                }
+
+            }
+
+            if (count == 0) {
+                return -1;
+            }
+        } return count;
     }
 }
