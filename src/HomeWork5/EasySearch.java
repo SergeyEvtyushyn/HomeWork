1f275a40e64837e98c50541aa620c60f92bc02eb
Sergey Evtyushyn <sergey.evtyushyn@gmail.com>
2022-05-18 00:50:30 +0300

    ДЗ 5. Обновленная задача 4.1 EasySearch и тест задача 6.

--- src/HomeWork5/EasySearch.java
+++ src/HomeWork5/EasySearch.java
@@ -13,18 +13,27 @@ public class EasySearch implements ISearchEngine{
      */
     @Override
     public long search(String text, String word){
-        String[] s = {};
         long count = 0;
+        long index = 0;
+        long index1 = 0;
 
-        if (word.length() == 1) { //Часть для поиска союзов или слов в одну букву
-            s = new String[]{" " + word + " ", " " + word + ".", " " + word + ",", " " + word + ":", " " + word + ";",
-                    word + " ", word + ".", word + ",", word + ":", word + ";", " " + word};
+        if (word.length() == 1) {
+            String[] s = new String[]{word + " ", word + ".", word + ",", word + ":",word + ";", word + "!", word + "?"}; // TODO... Пересмотреть вариант нахождения союза в начале строки
 
-            long index = text.indexOf(word);
             for (String words : s) {
+                index = 0;
+                index1 = 0;
                 while (index != -1) {
-                    index = text.indexOf(word, (int) (index + 1));
-                    count++;
+                    index = text.indexOf(words, (int) index1);
+                    index1 = index + 1;
+
+                    boolean a = index > 0;
+                    if ( a ) {
+                        char c1 = text.charAt((int) (index - 1));
+                        if (!Character.isDigit(c1) && !Character.isLetter(c1)) {
+                            count++;
+                        }
+                    }
                 }
             }
             if (count == 0) {
@@ -32,8 +41,6 @@ public class EasySearch implements ISearchEngine{
             }
 
         } else { //Часть для поиска целых слов
-            long index = 0;
-            long index1 = 0;
 
             while (index != -1) {
                 index = text.indexOf(word, (int) index1);
